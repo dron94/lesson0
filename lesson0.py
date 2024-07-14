50da2fc7583d2b6d7c354b660c96a13c7edb4d64
dron94 <andreypan1994@gmail.com>
2024-07-14 16:13:47 +0500

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,8 @@
+#1st program
+print(9**0.5*5)
+#2st program
+print(9.99>9.98 and 1000!=1000.1)
+#3st program
+print((((1234-4)/10)%100)+((5678-8)/10)%100)
+#4st program
+print(int(13.42) == int((42.13-42)*100) or int((13.42-13)*100) == int(42.13))
